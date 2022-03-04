55d3e0287451acaeff79d89a0ab9dd8e1605b8e1
Renato <renatopdamazio@gmail.com>
2022-03-04 15:28:34 -0300

    description

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Container, Content, CTA, CTALogoOne } from "./Login.styles";
+import {
+  Container,
+  Content,
+  CTA,
+  CTALogoOne,
+  Description,
+} from "./Login.styles";
 import { SignUp } from "../Buttons/Button.styles";
 import Image from "../Images/Image";
 
@@ -12,6 +18,11 @@ export default function Login() {
             <Image src="/images/cta-logo-one.svg" alt="CTA lOGO" />
           </CTALogoOne>
           <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
         </CTA>
       </Content>
     </Container>
--- src/components/Login/Login.styles.js
+++ src/components/Login/Login.styles.js
@@ -39,3 +39,11 @@ export const CTALogoOne = styled.figure`
     margin-bottom: 12px;
     min-height: 1px;
 `
+
+export const Description = styled.p`
+    color:  hsla(0, 0%,  95.3%, 1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5em;
+    letter-spacing: 1.5px;
+`
