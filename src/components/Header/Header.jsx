ab762f7312591adc427e1f75de4af31e77b7bf59
Renato <renatopdamazio@gmail.com>
2022-03-05 14:00:30 -0300

    Header menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,60 @@
 import React from "react";
-import { Nav } from "./Header.style";
+import { Nav, NavMenu, NavMenuItem } from "./Header.style";
 import Image from "../Images/Image";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const menus = [
+    {
+      title: "HOME",
+      url: "/",
+      icon: "home-icon.svg",
+    },
+    {
+      title: "SEARCH",
+      url: "/",
+      icon: "search-icon.svg",
+    },
+    {
+      title: "WATCHLIST",
+      url: "/",
+      icon: "watchlist-icon.svg",
+    },
+    {
+      title: "ORIGINALS",
+      url: "/",
+      icon: "original-icon.svg",
+    },
+    {
+      title: "MOVIES",
+      url: "/",
+      icon: "movie-icon.svg",
+    },
+    {
+      title: "SERIES",
+      url: "/",
+      icon: "series-icon.svg",
+    },
+  ];
   return (
     <Nav>
       <Image src="/images/logo.svg" width="80px" alt="Disney+" />
+
+      <NavMenu>
+        {menus.map((menu) => (
+          <NavMenuItem key={menu.title}>
+            <a href={menu.url}>
+              <Image
+                src={`/images/${menu.icon}`}
+                width="20px"
+                height="20px"
+                alt={menu.title}
+              />
+              <span>{menu.title}</span>
+            </a>
+          </NavMenuItem>
+        ))}
+      </NavMenu>
     </Nav>
   );
 };
--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -9,8 +9,63 @@ export const Nav = styled.nav`
     background-color: #090b13;
     display: flex;
     align-items: center;
-    justify-items: space-between;
+    justify-content: space-between;
     padding: 0 36px;
-    letter-spacing: 16px;
     z-index: 3;
+`
+
+export const NavMenu = styled.ul`
+    display: flex;
+    align-items: center;
+    flex-row: row nowrap;
+    flex-grow: 1;
+    height: 100%;
+    margin-left: 25px;
+
+    @media (max-width: 768px) {
+        display: none;
+    }
+`
+
+export const NavMenuItem = styled.li`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        position: relative;
+
+        &:hover span:before {
+            width: 100%;
+            opacity: 1;
+        }
+    }
+
+    span {
+        color: rgb(249, 249, 249);
+        font-size: 12px;
+        display: inline-block;
+        letter-spacing: 1.42px;
+        line-height: 1.08;
+        padding: 4px;
+        position: relative;
+        margin-left: 4px;
+
+        &:before {
+            content: "";
+            width: 0%;
+            height: 2px;
+            background: #fff;
+            left: 0;
+            position: absolute;
+            bottom: -4px;
+            opacity: 0;
+            border-radius: 2px;
+            transition: .250s ease-in-out;
+        }
+    }
+
 `
